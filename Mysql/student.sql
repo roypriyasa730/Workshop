93b08e9e0a3963b2a4e00524362a7f94de35739e
roypriyasa <roypriyasa730@gmail.com>
2025-06-17 23:05:46 +0530

    add additional fields to students table

--- Mysql/student.sql
+++ Mysql/student.sql
@@ -10,5 +10,11 @@ CREATE TABLE students (
     frist_name varchar(20),
     last_name varchar(10),
     dob date,
-   gmail varchar
+   gmail varchar,
+    address varchar(50),
+    city varchar(20),
+    state varchar(20),
+    zip_code int,
+    phone_number varchar(15),
+    PRIMARY KEY (id)
    );

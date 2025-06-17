8d40349bc9686a912c3588a14d4b8e3eff8620c2
roypriyasa <roypriyasa730@gmail.com>
2025-06-17 23:27:53 +0530

    add: insert initial data into students table

--- Mysql/student.sql
+++ Mysql/student.sql
@@ -24,3 +24,17 @@ show tables;
 
 -- Describe the structure of the students table
 describe students;
+
+-- Insert data into the students table
+INSERT INTO students (id, first_name, last_name, dob, gmail, address, city, state, zip_code, phone_number) 
+VALUES 
+  (1, 'Thamma', 'Roy', '2000-01-01','johndoe1@gmail.com', '123 Main St', 'Springfield', 'IL', 62701, '555-1234'),
+  (2, 'Amar', 'Roy', '1999-02-02','amarroy2@gmail.com', '456 Elm St', 'Springfield', 'IL', 62702, '555-5678'),
+  (3, 'Santana', 'Roy', '1998-03-03','sanatanaroy3@gmail.com', '789 Oak St', 'Springfield', 'IL', 62703, '555-9012'),
+  (4, 'Apu', 'Roy', '1997-04-04','apuroy4@gmail.com', '101 Pine St', 'Springfield', 'IL', 62704, '555-3456'),
+  (5, 'Tapu', 'Roy', '1996-05-05','tapuroy5@gmail.com','202 Maple St', 'Springfield', 'IL', 62705, '555-7890'),
+  (6, 'Papan', 'Roy', '1995-06-06','papanroy@gmail.com', '303 Cedar St', 'Springfield', 'IL', 62706, '555-2345'),
+  (7, 'Priyasa', 'Roy', '1994-07-07', 'priyasaroy7@gmail.com', '404 Birch St', 'Springfield', 'IL', 62707, '555-6789'),
+  (8, 'Aarohi', 'Roy', '1993-08-08','aarohiroy@gmail.com', '505 Walnut St', 'Springfield', 'IL', 62708, '555-0123'),
+  (9, 'Sonali', 'Roy', '1992-09-09','sonaliroy@gmail.com', '606 Chestnut St', 'Springfield', 'IL', 62709, '555-4567');
+

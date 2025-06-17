a3220918e72c9ebfaa7343007ad33aea78a4330e
roypriyasa <roypriyasa730@gmail.com>
2025-06-17 22:46:40 +0530

    Create table

--- Mysql/student.sql
+++ Mysql/student.sql
@@ -1,3 +1,10 @@
 -- Create database
 CREATE DATABASE student_db;
 USE student_db;
+CREATE TABLE students (
+    id int,
+    frist_name varchar(20),
+    last_name varchar(10),
+    dob date,
+   gmail varchar
+   );

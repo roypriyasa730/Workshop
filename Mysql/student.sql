09afe73e1119fafcf85205a2e5678258be3bb415
roypriyasa <roypriyasa730@gmail.com>
2025-06-17 22:56:04 +0530

    create student table

--- Mysql/student.sql
+++ Mysql/student.sql
@@ -1,6 +1,10 @@
 -- Create database
 CREATE DATABASE student_db;
+
+-- Select database 
 USE student_db;
+
+-- Create new student table
 CREATE TABLE students (
     id int,
     frist_name varchar(20),

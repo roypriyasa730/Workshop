0220ea4eb7654b6a3ee01ee8edce7cf296373d5d
roypriyasa <roypriyasa730@gmail.com>
2025-07-19 23:43:40 +0530

    add: query to count student attendance for each exam

--- Mysql/student.sql
+++ Mysql/student.sql
@@ -219,3 +219,12 @@ SELECT tweet_id
 FROM Tweets
 WHERE LENGTH(content) > 15;
 
+--Find the number of times each student attended each exam 
+
+
+SELECT s.student_id,s.student_name,u.subject_name, COUNT(e.subject_name) AS attended_exams
+FROM Students s
+JOIN Subjects u
+LEFT JOIN Examinations e ON s.student_id = e.student_id AND u.subject_name = e.subject_name
+GROUP BY s.student_id, u.subject_name
+ORDER BY s.student_id, u.subject_name;

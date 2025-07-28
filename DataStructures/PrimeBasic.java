546ec35f681f7032912dda59d7d95aff7151f38f
roypriyasa <roypriyasa730@gmail.com>
2025-07-28 23:34:28 +0530

    add: implement PrimeBasic class to check if a number is prime

---
+++ DataStructures/PrimeBasic.java
@@ -0,0 +1,25 @@
+public class PrimeBasic {
+    public static void main(String[] args) {
+        int number = 12;
+
+        boolean isPrime = true;
+
+        if (number <= 1) {
+            isPrime = false;
+        } else {
+
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(number + " is a Prime number.");
+        } else {
+            System.out.println(number + " is NOT a Prime number.");
+        }
+    }
+}

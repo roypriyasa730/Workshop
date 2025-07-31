5170f384e096f0ae28524dc53be28294da69e658
roypriyasa <roypriyasa730@gmail.com>
2025-08-01 01:52:37 +0530

    fix: format code in AddNumber.java for improved readability

--- DataStructures/AddNumber.java
+++ DataStructures/AddNumber.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
+
 public class AddNumber {
-public static void main(String[] args) {
+    public static void main(String[] args) {
         // Scanner should be created first
         Scanner input = new Scanner(System.in);
-        
+
         System.out.print("Enter first number: ");
         int num1 = input.nextInt();
 
-            System.out.print("Enter second number: ");
-            int num2 = input.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = input.nextInt();
 
-            int sum = num1 + num2;
+        int sum = num1 + num2;
 
-            System.out.println("Sum = " + sum);
-        }
-
+        System.out.println("Sum = " + sum);
     }
+
 }

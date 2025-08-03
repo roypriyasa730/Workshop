e80b834c17949df31316fe1de659657a0f3bb773
roypriyasa <roypriyasa730@gmail.com>
2025-08-03 21:18:21 +0530

    add: update .gitignore to exclude .DS_Store files and add Java project files; create Main class with student details and leap year check

---
+++ Java/src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void main(String[] args) {
+        String studentName = "Priyasa Roy";
+        int studentID = 3087;
+        int studentAge = 23;
+        float studentFee = 75.25f;
+        char studentGrade = 'B';
+
+        System.out.println("Hello world!");
+        // System.out.println(1000);
+
+        System.out.println("Student name: " + studentName);
+        System.out.println("Student id: " + studentID);
+        System.out.println("Student age: " + studentAge);
+        System.out.println("Student fee: " + studentFee);
+        System.out.println("Student grade: " + studentGrade);
+
+        int x = 5;
+        int y = 3;
+
+        if (5 < 3) {
+            System.out.println("x is greater than y");
+        } else {
+            System.out.println("x is not greater than y");
+        }
+
+        int year = 2024; // Example year
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " is a Leap Year.");
+        } else {
+            System.out.println(year + " is not a Leap Year.");
+        }
+
+        System.out.println(x - y);
+        System.out.println(x + y);
+
+        String FirstName = "Priyasa";
+        String LastName = "Roy";
+        System.out.println(FirstName + " " + LastName);
+
+    }
+}

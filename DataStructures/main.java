630759ea99acc5ccd89449b4a303b7350530c68a
roypriyasa <roypriyasa730@gmail.com>
2025-08-06 00:28:24 +0530

    add: implement main class to remove duplicates from a sorted array

---
+++ DataStructures/main.java
@@ -0,0 +1,20 @@
+public class main {
+    public static void main(String[] args) {
+        int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+        int k = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[k - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        // Output the result
+        //
+        System.out.println("k = " + k);
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}

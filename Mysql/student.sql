e5de1f9e5e7b12a1405ce48ac8334719f625ef3b
roypriyasa <roypriyasa730@gmail.com>
2025-09-15 00:32:15 +0530

    add: implement query to find direct reports and average age for each manager

--- Mysql/student.sql
+++ Mysql/student.sql
@@ -325,3 +325,14 @@ SELECT  x, y, z, CASE WHEN x + y > z AND x + z > y AND y + z > x
 THEN 'Yes' 
 ELSE 'No' END AS triangle
 FROM Triangle;
+
+
+
+-- Find the number of direct reports and the average age of direct reports for each manager
+SELECT  m.employee_id, m.name,
+    COUNT(e.employee_id) AS reports_count,
+    ROUND(AVG(e.age)) AS average_age
+FROM Employees e
+JOIN Employees m  ON e.reports_to = m.employee_id
+GROUP BY m.employee_id, m.name
+ORDER BY m.employee_id;

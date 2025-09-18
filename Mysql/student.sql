acb735dafca7b786d981dd04b8ec3b9337fee3ca
roypriyasa <roypriyasa730@gmail.com>
2025-09-18 20:45:42 +0530

    add: implement query to find confirmation rate for each user

--- Mysql/student.sql
+++ Mysql/student.sql
@@ -363,3 +363,12 @@ select name from Employee where id in
 
 where managerId is not null 
 group by managerId having count(*) > 4)
+
+-- Find the confirmation rate for each user
+SELECT s.user_id,
+    ROUND(
+        IFNULL(SUM(c.action = 'confirmed') / COUNT(c.action), 0),
+        2) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c  ON s.user_id = c.user_id
+GROUP BY s.user_id;

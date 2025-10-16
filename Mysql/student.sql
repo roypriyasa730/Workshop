24426fd8f1053da236d661f1041ed8ba001338ef
roypriyasa <roypriyasa730@gmail.com>
2025-10-17 00:06:21 +0530

    add: implement game play analysis query to calculate player engagement fraction

--- Mysql/student.sql
+++ Mysql/student.sql
@@ -492,3 +492,15 @@ SELECT
   SUM(IF(state = 'approved', amount, 0)) AS approved_total_amount
 FROM Transactions
 GROUP BY 1, 2;
+
+--Game Play Analysis IV (Question #550)
+
+WITH t1 AS (SELECT player_id,
+MIN(event_date) AS first_login_date
+FROM Activity
+GROUP BY player_id
+)
+SELECT ROUND(COUNT(*) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
+FROM Activity a1
+JOIN t1 t ON a1.player_id = t.player_id 
+AND a1.event_date = DATE_ADD(t.first_login_date , INTERVAL 1 DAY);

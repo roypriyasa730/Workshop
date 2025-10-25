8f0189685896308135b4e983fc9a26894b0b0989
roypriyasa <roypriyasa730@gmail.com>
2025-10-25 23:56:30 +0530

    fix: correct comparison logic in conditional statement

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -18,7 +18,7 @@ public class Main {
         int x = 5;
         int y = 3;
 
-        if (5 < 3) {
+        if (x > y) {
             System.out.println("x is greater than y");
         } else {
             System.out.println("x is not greater than y");

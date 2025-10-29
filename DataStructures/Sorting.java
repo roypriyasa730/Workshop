fcdf7dc4afdc460ae18b2095eaf59ba6490e66ce
roypriyasa <roypriyasa730@gmail.com>
2025-10-30 00:00:30 +0530

    fix: correct method declaration and remove extraneous braces in strStr implementation

---
+++ DataStructures/Sorting.java
@@ -0,0 +1,18 @@
+public public int strStr(String haystack, String needle) {
+             int m = haystack.length();   
+        int n = needle.length();     
+
+       
+        for (int i = 0; i <= m - n; i++) {
+           
+            String sub = haystack.substring(i, i + n);
+
+           
+            if (sub.equals(needle)) {
+                return i; 
+            }
+        }
+        return -1; 
+    } {
+    
+}

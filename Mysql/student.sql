46100adc1c519dba04668c7608c613d2cbffe6ed
roypriyasa <roypriyasa730@gmail.com>
2025-11-06 22:58:33 +0530

    feat: add query to find consecutive numbers in Logs table

--- Mysql/student.sql
+++ Mysql/student.sql
@@ -586,3 +586,10 @@ GROUP BY product_id
 ON s.product_id = first_sales.product_id 
 AND s.year = first_sales.first_year;
 
+--180. Consecutive Numbers
+SELECT DISTINCT A.Num AS ConsecutiveNums
+FROM Logs AS A
+INNER JOIN Logs AS B ON A.Id + 1 = B.Id
+INNER JOIN Logs AS C ON B.Id + 1 = C.Id
+WHERE A.Num = B.Num
+AND B.Num = C.Num;

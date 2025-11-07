cb4b172bd509434a74dab6a40ccb9d47bcfbcbb1
roypriyasa <roypriyasa730@gmail.com>
2025-11-07 22:11:20 +0530

    feat: add NextGreatestLetter class to find smallest letter greater than target

--- DataStructures/BinarySearch.java
+++ DataStructures/BinarySearch.java
@@ -32,3 +32,25 @@ public class BinarySearch {
         binarySearchIterative(arr, target);
     }
 }
+
+// NextGreatestLetter.java
+// LeetCode Problem: Find Smallest Letter Greater Than Target
+class NextGreatestLetter {
+
+    public char nextGreatestLetter(char[] letters, char target) {
+        int left = 0;
+        int right = letters.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (letters[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return letters[left % letters.length];
+    }
+}

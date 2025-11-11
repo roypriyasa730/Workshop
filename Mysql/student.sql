a037e42a00a0672519fc69241d41c0b6bf5586b8
roypriyasa <roypriyasa730@gmail.com>
2025-11-11 23:31:05 +0530

    feat: add game play analysis query to calculate average processing time

--- Mysql/student.sql
+++ Mysql/student.sql
@@ -594,4 +594,17 @@ INNER JOIN Logs AS C ON B.Id + 1 = C.Id
 WHERE A.Num = B.Num
 AND B.Num = C.Num;
 
-
+-- Game Play Analysis V (Question #551)
+SELECT
+  StartActivity.machine_id,
+  ROUND(
+    AVG(EndActivity.timestamp - StartActivity.timestamp),
+    3
+  ) AS processing_time
+FROM Activity AS StartActivity
+INNER JOIN Activity AS EndActivity
+  USING (machine_id, process_id)
+WHERE
+  StartActivity.activity_type = 'start'
+  AND EndActivity.activity_type = 'end'
+GROUP BY 1;

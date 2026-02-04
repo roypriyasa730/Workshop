aa86ac6cdbf5c5099e6a104021b4b4a0db2e7a7a
roypriyasa <roypriyasa730@gmail.com>
2026-02-04 22:58:10 +0530

    feat: implement NextGreatest class to find the next greatest letter

---
+++ DataStructures/NextGreatest.java
@@ -0,0 +1,19 @@
+public class NextGreatest {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int l = 0;
+        int r = letters.length;
+
+        while (l < r) {
+            final int m = (l + r) / 2;
+            if (letters[m] > target)
+                r = m;
+            else
+                l = m + 1;
+        }
+
+        return letters[l % letters.length];
+    }
+}
+
+// Compare this snippet from removeElement.java:
+// class Solution {

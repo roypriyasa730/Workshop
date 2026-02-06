f6cc5f97edaca4db07f998c059fcdc25f470e263
roypriyasa <roypriyasa730@gmail.com>
2026-02-07 00:26:23 +0530

    feat: implement countNegatives method to count negative numbers in a 2D grid

--- DataStructures/CountNegative.java
+++ DataStructures/CountNegative.java
@@ -1,8 +1,11 @@
 public class CountNegative {
 
     public int countNegatives(int[][] grid) {
-        int count = 0;
-
-        return count;
+        int n = nums.length;
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = nums[(((i + nums[i]) % n) + n) % n];
+        }
+        return res;
     }
 }

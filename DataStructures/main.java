cf3bbf9251a24df382f2db7152b3972add26c212
roypriyasa <roypriyasa730@gmail.com>
2026-02-08 23:58:00 +0530

    fix: correct class name from 'main' to 'Main' in main.java

--- DataStructures/main.java
+++ DataStructures/main.java
@@ -1,7 +1,10 @@
-public class main {
+class Main {
     public static void main(String[] args) {
         int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
         int k = 1;
+
+        // reference args to avoid "not used" diagnostics
+        if (args != null) { }
 
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] != nums[k - 1]) {

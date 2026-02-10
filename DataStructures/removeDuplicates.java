917211d2e84efec4c217404a8a43ae02a4642c8a
roypriyasa <roypriyasa730@gmail.com>
2026-02-10 23:19:48 +0530

    feat: add sorting to removeDuplicates method to ensure unique elements are adjacent

--- DataStructures/removeDuplicates.java
+++ DataStructures/removeDuplicates.java
@@ -1,8 +1,12 @@
+import java.util.Arrays;
+
 class Solution {
     public int removeDuplicates(int[] nums) {
         if (nums.length == 0) {
             return 0;
         }
+
+        Arrays.sort(nums);
 
         int i = 0;
         for (int j = 1; j < nums.length; j++) {

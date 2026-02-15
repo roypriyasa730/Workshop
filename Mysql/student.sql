a7ab901ce3660c027c34d677623124beaf1421da
roypriyasa <roypriyasa730@gmail.com>
2026-02-15 23:46:17 +0530

    fix: rename parameter in binarySearch method for clarity

--- Mysql/student.sql
+++ Mysql/student.sql
@@ -637,20 +637,19 @@ ORDER BY 1;
     }
 
    -- Helper function to perform binary search
-    private int binarySearch(int[] nums, int value) {
+    private int binarySearch(int[] nums, int target) {
         int left = 0;
         int right = nums.length;
-
+        
         while (left < right) {
+            int mid = left + (right - left) / 2;
             
-            int mid = (left + right) / 2;
-
-            if (nums[mid] >= value) {
-                right = mid;     
+            if (nums[mid] < target) {
+                left = mid + 1;
             } else {
-                left = mid + 1;  
+                right = mid;
             }
         }
-
+        
         return left;
     }

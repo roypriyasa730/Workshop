0db19525785aa18be5126f194485d2b6bbeca176
roypriyasa <roypriyasa730@gmail.com>
2026-02-21 23:52:35 +0530

    feat: add method to find the next greatest letter in a sorted array

--- Mysql/student.sql
+++ Mysql/student.sql
@@ -698,3 +698,21 @@ GROUP BY 1, 2;
         return prefix;
     }
 }
+
+
+Class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int l = 0;
+    int r = letters.length;
+
+    while (l < r) {
+      final int m = (l + r) / 2;
+      if (letters[m] > target)
+        r = m;
+      else
+        l = m + 1;
+    }
+
+    return letters[l % letters.length];
+  }
+    }
